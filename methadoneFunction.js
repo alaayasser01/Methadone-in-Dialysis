3e9a39089bb71e96e331d685df9380c7bc551766
alaayasser01 <alaayasser499@gmail.com>
2022-01-03 15:30:47 +0200

    again

--- methadoneFunction.js
+++ methadoneFunction.js
@@ -6,9 +6,10 @@ let out = 0 ;
 document.getElementById("result").addEventListener("click", myFunction);
 
 function myFunction() {
-    var u = parseFloat (document.getElementById('Uvar').value);
-    var z = parseFloat (document.getElementById('Zvar').value) ;
-    let m = 8.451566103*Math.pow(10, -5);
-    out = u* Math.exp(-1*m*z);
-    document.getElementById('output').append("The concentration of the Methadone after dialysis =" + out )
-  }
+  var u = parseFloat (document.getElementById('Uvar').value);
+  var z = parseFloat (document.getElementById('Zvar').value) ;
+  let m = 8.451566103*Math.pow(10, -5);
+  out = u* Math.exp(-1*m*z);
+  
+    document.getElementById('output').innerText =("The concentration of the Methadone after dialysis =" + out + " nm/ml" )
+    }
